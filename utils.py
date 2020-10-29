091d0b919a9752140b27916752cfb2341638a1b9
SiddhanthHegde <siddhanthhegde227@gmail.com>
2020-10-30 02:20:49 +0530

    Uploaded utils for the main code

---
+++ utils.py
@@ -0,0 +1,27 @@
+import torch
+from PIL import Image
+
+def load_image(image_name):
+    image = Image.open(image_name)
+    image = transform(image).unsqueeze(0)
+    return image.to(device)
+
+
+def content_loss_calc(gen_image,org_image):
+  squared = (gen_image - org_image) ** 2
+  return torch.mean(squared)
+
+def style_loss_calc(gen_image,style_image,channel,width,height):
+  G = gen_image.view(channel, height * width).mm(
+      gen_image.view(channel,height * width).t()
+  )
+
+  A = style_image.view(channel, height * width).mm(
+      style_image.view(channel, height * width).t()
+  )
+
+  squared = (G - A) ** 2
+  return torch.mean(squared)
+
+
+
