53126a1197ed212766ff20a2e91d17e029f5b301
SiddhanthHegde <siddhanthhegde227@gmail.com>
2020-11-04 00:07:10 +0530

    device added for utils.py file

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 transform = transforms.Compose(
     [
      transforms.Resize((316,474)),# shape of used content image
